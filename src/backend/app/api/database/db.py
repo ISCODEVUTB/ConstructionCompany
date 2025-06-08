78a88b42ab24e3c134d14fccaf67aaa3926a3d8b
Gabrielleon0215 <leong@utb.edu.co>
2025-06-08 04:00:46 +0000

    backend deployment

--- src/backend/app/api/database/db.py
+++ src/backend/app/api/database/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, Session
-from backend.app.api.database.schemas.users_db import Base
+from src.backend.app.api.database.schemas.users_db import Base
 import os
 from dotenv import load_dotenv
 from sqlalchemy.pool import StaticPool
